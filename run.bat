2538d713f5dcc7cff246ea3a36e7587e940747e8
ThatonemusicianF <120556060+ThatonemusicianF@users.noreply.github.com>
2025-02-13 23:42:31 +1300

    Hi logan! this will be a quick intro script that should also install libs/APIs needed.

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+echo So...this is a project that basically is done by 2 random ass devs for fun.
+echo This is also used for installing libraries for python. 
+echo You will need python to use this.
+echo Thank you for reading!
+REM wget install 
+
+
+echo All done installing!
