dbcb8be78426011e3870d771f5d2972c8a282556
Jack <120556060+izeperson@users.noreply.github.com>
2025-02-15 13:28:24 +1300

    run.bat timeout added

--- run.bat
+++ run.bat
@@ -6,6 +6,10 @@ echo So...this is a project that basically is done by 2 random ass devs for fun.
 echo This is also used for installing libraries for python.
 echo You will need Python to use this.
 
+echo Python Installing in 2 Seconds.
+
+timeout /t 2 >nul
+
 winget install python --disable-interactivity
 
 echo Python install checked!
